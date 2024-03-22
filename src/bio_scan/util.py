414fe63a4f20f60124902be9989514b6aa8399ee
Jeremy Rimpo <jrim@rimpo.org>
2024-03-22 04:28:01 -0500

    Fix typo

--- src/bio_scan/util.py
+++ src/bio_scan/util.py
@@ -3,5 +3,5 @@ import math
 
 def system_distance(coordinate_a: tuple[float, float, float], coordinate_b: tuple[float, float, float]) -> float:
     return math.sqrt((coordinate_a[0] - coordinate_b[0]) ** 2
-                     + (coordinate_a[1] - coordinate_b[0]) ** 2
-                     + (coordinate_a[2] - coordinate_b[0]) ** 2)
+                     + (coordinate_a[1] - coordinate_b[1]) ** 2
+                     + (coordinate_a[2] - coordinate_b[2]) ** 2)
