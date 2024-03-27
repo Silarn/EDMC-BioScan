ddd1577e8d2626ba7e3e295d0f91eaa072d6098a
Jeremy Rimpo <jrim@rimpo.org>
2024-03-27 17:17:40 -0500

    Adjust shard requirements based on codex info

--- src/bio_scan/bio_data/rulesets/shard.py
+++ src/bio_scan/bio_data/rulesets/shard.py
@@ -11,9 +11,8 @@ catalog: dict[str, dict[str, Mapping]] = {
                                    'Helium', 'Methane', 'Neon', 'NeonRich'],
                     'max_gravity': 2.0,
                     'max_temperature': 273.0,
-                    'star': ['F', 'G', 'K'],
-                    'distance': 10000.0,
-                    'life_plus': True,
+                    'star': ['A', 'F', 'G', 'K', 'MS', 'S'],
+                    'distance': 12000.0,
                     'bodies': ['Earthlike body', 'Ammonia world', 'Water world', 'Gas giant with water based life',
                                'Gas giant with ammonia based life', 'Water giant'],
                     'regions': ['exterior']
